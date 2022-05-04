a0d365f7b6bfb6528d4a08da42a34d8846d5614e
Anders Børjesson <anbo@zealand.dk>
2022-05-04 13:10:14 +0200

    Add project files.

---
+++ IsSorted/Order.cs
@@ -0,0 +1,45 @@
+﻿namespace IsSorted
+{
+    /// <summary>
+    /// May be useful with unit tests.
+    /// NUnit has a similar function https://csharp.hotexamples.com/examples/NUnit.Framework/List/IsSorted/php-list-issorted-method-examples.html
+    /// Uses generic extension method with default parameter + constraints on the type parameter, and method overloading
+    /// </summary>
+    // TODO needs unit testing
+    static class Order
+    {
+        // https://www.dotnetperls.com/issorted
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/constraints-on-type-parameters
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods
+        public static bool IsSorted<T>(this T[] arr, bool desc = false) where T : IComparable<T>
+        {
+            if (desc) return IsSortedDescending(arr); // TODO looking for smarter implementation
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1].CompareTo(arr[i]) > 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsSortedDescending<T>(this T[] arr) where T : IComparable<T>
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1].CompareTo(arr[i]) < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsSorted<T>(this IEnumerable<T> data, bool desc = false) where T : IComparable<T>
+        {
+            T[] arr = data.ToArray();
+            return IsSorted(arr, desc);
+        }
+    }
+}
---
+++ IsSorted/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IsSorted
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int[] arr = { 12, 63, 8, 6 };
+
+            bool isSorted = arr.IsSorted();
+            Console.WriteLine("Sorted " + isSorted);
+
+            int[] arr2 = { 1, 2, 3, 6, 19 };
+            Console.WriteLine(arr2.IsSorted());
+
+            string[] names = { "Anders", "Carl", "Dennis" };
+            Console.WriteLine("names " + names.IsSorted());
+
+            string[] names2 = { "Anders", "Carl", "Dennis", "Abraham" };
+            Console.WriteLine("names " + names2.IsSorted());
+
+            string[] namesReverse = { "Carl", "Benny", "Anders" };
+            Console.WriteLine("names reverse " + namesReverse.IsSorted(true));
+
+            List<int> list1 = new() { 4, 65, 6, 2 };
+            Console.WriteLine("list1 " + list1.IsSorted());
+            Console.WriteLine("list1 " + list1.IsSorted(true));
+        }
+    }
+}
